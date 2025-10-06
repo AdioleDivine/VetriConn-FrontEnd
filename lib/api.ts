dff2b9c7da1cdef57b3ef68ca71e5d192e592f37
AdioleDivine <adioledivine27@gmail.com>
2025-10-06 01:56:18 -0400

    updated icons

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_URL ||
   process.env.NEXT_PUBLIC_API_URL_DEV ||
-  "http://localhost:5000/api/v1";
+  "https://backend-bnm8.onrender.com/api/v1";
 
 // API Response types
 export interface LoginResponse {
